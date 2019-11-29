12e3abc5263639b530b132e7b8a5233710176e82
Vadim Flaks <Max@Vadims-MacBook-Pro.local>
2019-11-29 12:48:29 -0600

    Adjusting structure and README

--- engine/template_builder.py
+++ engine/template_builder.py
@@ -1,5 +1,26 @@
+import json
+
 self_closing_tags = ("area", "base", "br", "col",
                      "embed", "hr", "img", "input",
                      "link", "meta", "param", "source",
                      "track", "wbr")
-print("div" in self_closing_tags)
+
+
+class Tag():
+    def __init__(
+        self,
+        tag,
+        tag_id=None,
+        class_list=[]
+    ):
+        self.tag = tag
+        self.tag_id = tag_id
+        self.clas_list = class_list
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def export_tag(self):
+        tag_dict = {}
+        return json.dump(tag_dict)
